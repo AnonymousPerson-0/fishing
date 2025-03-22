de9dada78ed3391d55b4f87ef9a24ab25391abba
AnonymousPerson-0 <122753028+AnonymousPerson-0@users.noreply.github.com>
2025-03-22 16:56:10 -0700

    Update settings.py

--- game/settings.py
+++ game/settings.py
@@ -1,7 +1,11 @@
 WIDTH = 800
 HEIGHT = 600
 
-probabilities = {0 : [0.5, 1]}
-fishByStage = {0 : ["Normal Fish", "Rare Fish"]}
+probabilities = {0 : [0.7, 0.9, 1]}
+fishByStage = {0 : ["Normal Fish", "Rare Fish", "Elongated Fish"]}
 
-objects = {"Normal Fish": ["Your average fish.", 2], "Rare Fish": ["Slightly more valuable fish.", 5]} # each array is [string descriptor, value, category, effects]
+objects = {"Normal Fish": ["Your average fish.", 2, "Fish", "NONE"], 
+           "Rare Fish": ["Slightly more valuable fish.", 5, "Fish", "NONE"],
+           "Elongated Fish":["A fish that contains a valuable medicinal ingredient that can be used to cure PNEUMONIA or help treat CORRUPTION.", 10, "Key Fish", "Can be sacrificed."]
+}
+# each array is [string descriptor, value, category, effects]
